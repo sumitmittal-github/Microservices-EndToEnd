d130557405c6b0979ea3779cb6d384fedec9c392
Sumit Mittal <sumitmittal.adonis@gmail.com>
2025-04-06 13:11:43 -0400

    Code reorganized

--- accounts/src/main/java/com/sumit/accounts/dto/AccountDto.java
+++ accounts/src/main/java/com/sumit/accounts/dto/AccountDto.java
@@ -1,25 +1,20 @@
 package com.sumit.accounts.dto;
 
-import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
-@Schema(name = "Accounts", description = "Schema to hold Account information")
 public class AccountDto {
 
     @NotEmpty(message = "AccountNumber can not be a null or empty")
     @Pattern(regexp="(^$|[0-9]{10})",message = "AccountNumber must be 10 digits")
-    @Schema(description = "Account Number of Sumit Mittal Bank", example = "3454433243")
     private Long accountNumber;
 
     @NotEmpty(message = "AccountType can not be a null or empty")
-    @Schema(description = "Account type of Sumit Mittal Bank", example = "Savings")
     private String accountType;
 
     @NotEmpty(message = "BranchAddress can not be a null or empty")
-    @Schema(description = "Sumit Mittal Bank branch address", example = "123 NewYork")
     private String branchAddress;
 
 
--- accounts/src/main/java/com/sumit/accounts/dto/ResponseDto.java
+++ accounts/src/main/java/com/sumit/accounts/dto/ResponseDto.java
@@ -1,18 +1,13 @@
 package com.sumit.accounts.dto;
 
-import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
-@Schema(name = "Response", description = "Schema to hold successful response information")
 @Data
 @AllArgsConstructor
 public class ResponseDto {
 
-    @Schema(description = "Status code in the response")
     private int statusCode;
-
-    @Schema(description = "Status message in the response")
     private String statusMsg;
     
 }
